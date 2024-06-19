d8d9107fb93acb02935c03393bbe4cf587475474
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-06-19 09:02:57 -0500

    Update movie_removal.jsx

--- movie_removal.jsx
+++ movie_removal.jsx
@@ -4,7 +4,7 @@ const MoviesList = () => {
     const [movies, setMovies] = useState([
         { id: 1, title: 'Harriet the Spy', genre: 'Drama', description: 'Follows 11-year-old Harriet M. Welsch, an aspiring writer whose life turns upside down when her classmates discover her brutally honest observations about them in a secret notebook.' },
         { id: 2, title: 'Spiderman', genre: 'Sci-Fi', description: 'Chronicles the transformation of high school student Peter Parker into the iconic web-slinging superhero after being bitten by a genetically-modified spider.' },
-        { id: 3, title: 'Hidden Figures', genre: 'Action', description: 'Three brilliant African-American women mathematicians played pivotal roles in NASA during the early years of the U.S. space program.' }
+        { id: 3, title: 'Hidden Figures', genre: 'Biography', description: 'Three brilliant African-American women mathematicians played pivotal roles in NASA during the early years of the U.S. space program.' }
     ]);
 
     const [showDetails, setShowDetails] = useState({});
